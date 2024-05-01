961b82dc02014def517d6f3bb6c2b10edbe7a741
YunHaoLi <yh2der@gmail.com>
2024-05-01 22:22:30 +0800

    May 1th 2024

---
+++ NeetCode150/Linked List/143. Reorder List/README.md
@@ -0,0 +1,20 @@
+# [143. Reorder List](https://leetcode.com/problems/reorder-list/description/)
+
+## UMPIRE
+### Understand
+反轉一個Linked List
+### Match
+* Linked List
+### Plan
+* 先用快慢指針找到中間節點
+* 將後半段那串list反轉
+* 最後將兩個list合併
+### Implement
+see sol.cpp
+### Review
+
+### Evaluate
+* Time Complexity O(n)
+* Space Complexity O(1)
+## Notes
+* 用快慢指針的技巧可以找到list的中間點
---
+++ NeetCode150/Linked List/143. Reorder List/sol.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) return;
+        
+        // Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // Reverse the second half of the list
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+        while (curr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        slow->next = nullptr;
+        
+        // Merge two lists
+        ListNode* first = head;
+        ListNode* second = prev;
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
